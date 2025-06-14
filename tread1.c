0856ebe2a28303bb238c9ee7491e1cfd969c7e0d
bil-sousa <153782558+bil-sousa@users.noreply.github.com>
2025-06-14 18:58:12 -0300

    Update tread1.c

--- tread1.c
+++ tread1.c
@@ -1,34 +1,43 @@
-/*
-  Criação de processos em UNIX.
+#include <unistd.h>      // Para fork(), getpid(), getppid(), etc.
+#include <stdio.h>       // Para printf(), scanf()
+#include <stdlib.h>      // Para exit()
+#include <sys/types.h>   // Para tipos como pid_t
+#include <sys/wait.h>    // Para wait()
 
-gcc -o fork fork.c
-*/
+int main() {
+    int retval, n;
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+    // Solicita ao usuário o número de filhos
+    printf("Digite o número de filhos a serem criados: ");
+    scanf("%d", &n);
 
-int main ()
-{
-  int retval, n;
+    // Imprime o PID do processo pai
+    printf("Olá, sou o processo %5d\n", getpid());
 
-  scanf("%d", &n);
-   
-  printf ("Ola, sou o processo %5d\n", getpid());
+    // Loop para criar N filhos
+    for (int i = 0; i < n; i++) {
+        retval = fork();  // Cria um novo processo filho
 
-  for(int i = 0; i < n; i++){
-        retval = fork ();
-        if(retval == 0){
-                printf ("[%d]-> sou %5d, filho de %5d\n", i, getpid(), getppid());
-                exit(0);
-        } else {
-                wait(0);
+        if (retval < 0) {
+            // Se houve erro ao criar o processo
+            perror("Erro ao criar processo");
+            exit(1);
         }
 
-}
+        if (retval == 0) {
+            // Código executado pelo processo filho
+            printf("[%d] -> sou %5d, filho de %5d\n", i, getpid(), getppid());
 
+            // Filho termina imediatamente após imprimir
+            exit(0);
+        } else {
+            // Código executado pelo processo pai
 
-  exit (0) ;
+            // Pai espera o filho terminar antes de continuar
+            wait(NULL);
+        }
+    }
+
+    // Após criar e esperar todos os filhos, o pai encerra
+    exit(0);
 }
